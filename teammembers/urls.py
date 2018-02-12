be11ac40f48e29ba5cd907cefc672ef8b2538414
ckoneti <ckoneti@castlighthealth.com>
2018-02-12 13:38:49 +0530

    Added a view for adding a new team member and related urls to urls.py

--- teammembers/urls.py
+++ teammembers/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-		url(r'^members/$', views.members, name='members'),		
+		url(r'^members/$', views.members, name='members'),
+		url(r'^addmember/$', views.add_member, name='add_member'),
 ]
--- teammembers/views.py
+++ teammembers/views.py
@@ -1,6 +1,8 @@
 import json
 from django.core import serializers
-from django.http import HttpResponse
+from django.db.utils import DataError
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 from teammembers.models import Member
 
 # Below method translates list of query-able objects to json object
@@ -19,3 +21,18 @@ def members(request):
 	members_list = Member.objects.all()
 	json_array = get_json(members_list)
 	return HttpResponse(json_array)
+
+@csrf_exempt
+def add_member(request):
+	try:
+		m = Member(first_name = request.POST.get('first_name')
+			, last_name = request.POST.get('last_name')
+			, phone_number = request.POST.get('phone_number')
+			, email = request.POST.get('email')
+			, role = request.POST.get('role')
+			)
+		m.save()
+		output = get_json(Member.objects.filter(id=m.id))
+		return HttpResponse(output)
+	except DataError:
+		return HttpResponseBadRequest('Data Error occured; Please check the data you have entered is per standards.\n')
