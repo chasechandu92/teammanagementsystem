ac107e4df53cdbe85c5861276ce3a1620533b7e3
ckoneti <ckoneti@castlighthealth.com>
2018-02-12 13:12:22 +0530

    Added a view to fetch all team members json array and related urls to urls.py

--- teammembers/views.py
+++ teammembers/views.py
@@ -1,6 +1,21 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import json
+from django.core import serializers
+from django.http import HttpResponse
+from teammembers.models import Member
 
-from django.shortcuts import render
+# Below method translates list of query-able objects to json object
+def get_json(list_of_objects):
+	serialized_data = serializers.serialize('python', list_of_objects)
 
-# Create your views here.
+	for item in serialized_data:
+		item['fields']['id'] = item['pk']
+
+	serialized_data_with_id = [item['fields'] for item in serialized_data]
+	json_array = json.dumps(serialized_data_with_id)
+	return json_array
+
+# Below view/endpoint returns a json array of all team members
+def members(request):
+	members_list = Member.objects.all()
+	json_array = get_json(members_list)
+	return HttpResponse(json_array)
