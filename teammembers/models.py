8a1bad0c88f6e106a8b6cb8ac9e83a81bb0dd400
ckoneti <ckoneti@castlighthealth.com>
2018-02-12 12:47:02 +0530

    Member model has been created; Used django_mysql module to get enum field

--- teammembers/models.py
+++ teammembers/models.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django_mysql.models import EnumField
 
-# Create your models here.
+class Member(models.Model):
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	phone_number = models.CharField(max_length=20)
+	email = models.EmailField()
+	role = EnumField(choices=['admin', 'regular'])
+
+	def __str__(self):
+		return self.first_name+', '+self.email
